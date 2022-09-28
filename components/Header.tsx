92e83d049b4ee3148ad24e065d380a618aa85ca2
jon <joao.couto@mindswap.academy>
2022-09-28 14:51:18 +0100

    project init

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Header: NextPage = () => {
+  return <div>TODO LIST, hora local</div>;
+};
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head"; //para meta info
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Todo App</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
