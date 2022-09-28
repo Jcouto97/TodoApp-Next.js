86d452bfc1ac5e4863498d4c2d523b410614db38
jon <joao.couto@mindswap.academy>
2022-09-28 16:06:42 +0100

    styled components fixed with babel file

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { Container } from "../styles/Header";
+import { Container } from "../styles/Header.styles";
 
 const Header: NextPage = () => {
   return <Container>TODO LIST, hora local</Container>;
