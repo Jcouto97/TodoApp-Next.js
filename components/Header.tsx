3010136356a6db86b9c5525c7a91d2a535dbc308
jon <joao.couto@mindswap.academy>
2022-09-28 20:13:20 +0100

    to do list with time stamped in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,44 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { Container } from "../styles/Header.styles";
 
-const Header: NextPage = () => {
+const Header = () => {
+  /*   if (message === "Login successful!") {
+    setTimeout(() => {
+      navigate("/bookspage");
+    }, 2000);
+  }
+ */
+
+  /*   const updatedTime = () =>{
+    setTimeout(() => {
+      return  new Date().toString()
+     
+    }, 1000) as any
+  }
+ */
+
+  const [time, setTime] = useState<string>(new Date().toString());
+
+  function displayTime() {
+    setTime(new Date().toString());
+  }
+
+  setTimeout(() => {
+    displayTime();
+  }, 1000);
+
   return (
-  <Link href='/'>
-  <Container>TODO LIST, hora local</Container> 
-  </Link>
-  )
+    <>
+      <Link href="/">
+        <Container>
+          <h1>TODO LIST</h1>
+          <p>{time}</p>
+        </Container>
+      </Link>
+    </>
+  );
 };
 
 export default memo(Header);
